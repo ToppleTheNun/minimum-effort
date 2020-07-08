aa764441548c703223a44f1d5e97a4be0ce893aa
Richard Harrah <topplethenunnery@gmail.com>
2020-07-07 20:25:15 -0400

    switch to gatsby instead of create-react-app

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero: React.FC = () => (
+  <section className="hero is-primary is-bold is-fullheight-with-navbar">
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title">Minimum Effort</h1>
+        <h2 className="subtitle">Semi-Hardcore Raiding Guild on Thrall</h2>
+        <h2 className="subtitle">Tuesday and Thursday from 8-11PM EST</h2>
+      </div>
+    </div>
+  </section>
+);
+
+export default Hero;
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type NavbarLinkJson = {
+  name: string;
+  href: string;
+};
+export type NavbarLinksJson = {
+  startLinks: NavbarLinkJson[];
+  endLinks: NavbarLinkJson[];
+};
