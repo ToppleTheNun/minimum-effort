0fbc09a9689f5da4b081156a22a013f8bc5ad169
Richard Harrah <topplethenunnery@gmail.com>
2020-07-07 21:28:09 -0400

    put button in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,6 +7,12 @@ const Hero: React.FC = () => (
         <h1 className="title">Minimum Effort</h1>
         <h2 className="subtitle">Semi-Hardcore Raiding Guild on Thrall</h2>
         <h2 className="subtitle">Tuesday and Thursday from 8-11PM EST</h2>
+        <a
+          className="button is-inverted is-large is-outlined is-primary"
+          href="https://forms.gle/dtNBBHvuDTSDtziU6"
+        >
+          Apply Now
+        </a>
       </div>
     </div>
   </section>
