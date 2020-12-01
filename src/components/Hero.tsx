8c7f006c335e61f8abda88dca8e08a442a71110c
Richard Harrah <topplethenunnery@gmail.com>
2020-11-30 22:15:34 -0500

    update recruitment form

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,15 +5,13 @@ const Hero: React.FC = () => (
     <div className="hero-body">
       <div className="container">
         <h1 className="title text-white">Minimum Effort</h1>
-        <h2 className="subtitle text-white">
-          Semi-Hardcore Raiding Guild on Thrall (Horde)
-        </h2>
+        <h2 className="subtitle text-white">Thrall (Horde)</h2>
         <h2 className="subtitle text-white">
           Tuesday, Wednesday, and Thursday from 8-11PM EST
         </h2>
         <a
           className="button is-inverted is-large is-outlined is-primary"
-          href="https://forms.gle/sRnhWeyeqtn4VLB48"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSccN2RsqWGMNWG8mKunlsf0x-cS8aZQupGktD9m8f89UAE_aQ/viewform"
         >
           Apply Now
         </a>
